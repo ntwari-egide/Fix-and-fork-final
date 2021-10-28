7e90434a76c719136c0f2de9f33de0b530e6f0b4
ntwari egide <ntwariegide2@gmail.com>
2021-10-27 22:40:36 -0700

    ft(styles)L add responsiveness

--- pages/blog/index.js
+++ pages/blog/index.js
@@ -36,41 +36,55 @@ const Home = ({
 
                 <Row className="pad64 blog-container">
                     <Col span={4}  className="action-container">
-                        <Tooltip placement="right" title={"Fork solution"}>
-                            <div className="action">
-                                <svg id="git-pull-request-line" xmlns="http://www.w3.org/2000/svg" width="29" height="29" viewBox="0 0 29 29">
-                                    <path id="Path_28" data-name="Path 28" d="M0,0H29V29H0Z" fill="none"/>
-                                    <path id="Path_29" data-name="Path 29" d="M18.333,5.789h2.555a2.541,2.541,0,0,1,2.555,2.526v10.32a3.828,3.828,0,1,1-2.555,0V8.316H18.333v3.789l-5.75-5.053L18.333,2ZM5.556,10.627a3.8,3.8,0,1,1,5.057-4.215,3.786,3.786,0,0,1-2.5,4.215v8.008a3.8,3.8,0,1,1-5.057,4.215,3.786,3.786,0,0,1,2.5-4.215ZM6.834,8.316A1.263,1.263,0,1,0,5.556,7.053,1.271,1.271,0,0,0,6.834,8.316Zm0,15.158A1.263,1.263,0,1,0,5.556,22.21,1.271,1.271,0,0,0,6.834,23.474Zm15.333,0a1.263,1.263,0,1,0-1.278-1.263A1.271,1.271,0,0,0,22.166,23.474Z" transform="translate(0 0)"/>
-                                </svg>
+                        <div className="post-actions-container">
+                            <Tooltip placement="right" title={"Fork solution"}>
+                                <div className="action">
+                                    <div className="ratings-container">
+                                        89
+                                    </div>
+                                    <svg id="git-pull-request-line" xmlns="http://www.w3.org/2000/svg" width="29" height="29" viewBox="0 0 29 29">
+                                        <path id="Path_28" data-name="Path 28" d="M0,0H29V29H0Z" fill="none"/>
+                                        <path id="Path_29" data-name="Path 29" d="M18.333,5.789h2.555a2.541,2.541,0,0,1,2.555,2.526v10.32a3.828,3.828,0,1,1-2.555,0V8.316H18.333v3.789l-5.75-5.053L18.333,2ZM5.556,10.627a3.8,3.8,0,1,1,5.057-4.215,3.786,3.786,0,0,1-2.5,4.215v8.008a3.8,3.8,0,1,1-5.057,4.215,3.786,3.786,0,0,1,2.5-4.215ZM6.834,8.316A1.263,1.263,0,1,0,5.556,7.053,1.271,1.271,0,0,0,6.834,8.316Zm0,15.158A1.263,1.263,0,1,0,5.556,22.21,1.271,1.271,0,0,0,6.834,23.474Zm15.333,0a1.263,1.263,0,1,0-1.278-1.263A1.271,1.271,0,0,0,22.166,23.474Z" transform="translate(0 0)"/>
+                                    </svg>
+                                    </div>
+                            </Tooltip>
+                            <Tooltip placement="right" title={"Total views"}>    
+                                <div className="action">
+                                    <div className="ratings-container">
+                                        29
+                                    </div>
+                                    <GrView color="#3A3E3E" />
                                 </div>
-                        </Tooltip>
-                        <Tooltip placement="right" title={"Total views"}>    
-                            <div className="action">
-                            <GrView color="#3A3E3E" />
-                            </div>
-                        </Tooltip>
+                            </Tooltip>
 
-                        <Tooltip placement="right" title={"Like solution"}>
-                            <div className="action">
-                                <BiHeart color="#3A3E3E" />
-                            </div>
-                        </Tooltip>
+                            <Tooltip placement="right" title={"Like solution"}>
+                                <div className="action">
+                                    <div className="ratings-container">
+                                        45
+                                    </div>
+                                    <BiHeart color="#3A3E3E" />
+                                </div>
+                            </Tooltip>
 
-                        <Tooltip  placement="right" title={"Total comments"}>
-                            <div className="action">
-                                <svg id="chat-1-line" xmlns="http://www.w3.org/2000/svg" width="29" height="29" viewBox="0 0 29 29">
-                                    <path id="Path_32" data-name="Path 32" d="M0,0H29V29H0Z" fill="none"/>
-                                    <path id="Path_33" data-name="Path 33" d="M12,3h5a10.051,10.051,0,0,1,0,20.1v4.4C10.75,24.987,2,21.218,2,13.051A10.026,10.026,0,0,1,12,3Zm2.5,17.59H17A7.539,7.539,0,0,0,17,5.513H12a7.519,7.519,0,0,0-7.5,7.538c0,4.536,3.077,7.5,10,10.654Z" transform="translate(0 0)"/>
-                                </svg>
+                            <Tooltip  placement="right" title={"Total comments"}>
+                                <div className="action">
+                                    <div className="ratings-container">
+                                        3
+                                    </div>
+                                    <svg id="chat-1-line" xmlns="http://www.w3.org/2000/svg" width="29" height="29" viewBox="0 0 29 29">
+                                        <path id="Path_32" data-name="Path 32" d="M0,0H29V29H0Z" fill="none"/>
+                                        <path id="Path_33" data-name="Path 33" d="M12,3h5a10.051,10.051,0,0,1,0,20.1v4.4C10.75,24.987,2,21.218,2,13.051A10.026,10.026,0,0,1,12,3Zm2.5,17.59H17A7.539,7.539,0,0,0,17,5.513H12a7.519,7.519,0,0,0-7.5,7.538c0,4.536,3.077,7.5,10,10.654Z" transform="translate(0 0)"/>
+                                    </svg>
 
-                            </div>
-                        </Tooltip>
+                                </div>
+                            </Tooltip>
 
-                        <Tooltip placement="right" title={"Github Link"}>
-                            <div className="action">
-                                <GrGithub color="#3A3E3E" />
-                            </div>
-                        </Tooltip>
+                            <Tooltip placement="right" title={"Github Link"}>
+                                <div className="action">
+                                    <GrGithub color="#3A3E3E" />
+                                </div>
+                            </Tooltip>
+                        </div>
                     </Col>
                     <Col span={16} className="main-post-content">
 
@@ -109,21 +123,24 @@ const Home = ({
                     
                     <Col span={4}  className="action-container right-actions">
 
-                        <Tooltip placement="left" title={"Present Solution"}>
-                            <div className="action">
-                                <svg id="slideshow-2-line" xmlns="http://www.w3.org/2000/svg" width="29" height="29" viewBox="0 0 29 29">
-                                    <path id="Path_34" data-name="Path 34" d="M0,0H29V29H0Z" fill="none"/>
-                                    <path id="Path_35" data-name="Path 35" d="M15.75,20.75V24.5H22V27H7V24.5h6.25V20.75H4.5A1.25,1.25,0,0,1,3.25,19.5V4.5H2V2H27V4.5H25.75v15a1.25,1.25,0,0,1-1.25,1.25Zm-10-2.5h17.5V4.5H5.75ZM12,7l6.25,4.375L12,15.75Z" fill="#535353"/>
-                                </svg>
+                        <div className="post-actions-container">
+                            <Tooltip placement="left" title={"Present Solution"}>
+                                <div className="action">
+                                    <svg id="slideshow-2-line" xmlns="http://www.w3.org/2000/svg" width="29" height="29" viewBox="0 0 29 29">
+                                        <path id="Path_34" data-name="Path 34" d="M0,0H29V29H0Z" fill="none"/>
+                                        <path id="Path_35" data-name="Path 35" d="M15.75,20.75V24.5H22V27H7V24.5h6.25V20.75H4.5A1.25,1.25,0,0,1,3.25,19.5V4.5H2V2H27V4.5H25.75v15a1.25,1.25,0,0,1-1.25,1.25Zm-10-2.5h17.5V4.5H5.75ZM12,7l6.25,4.375L12,15.75Z" fill="#535353"/>
+                                    </svg>
 
-                            </div>
-                        </Tooltip>
+                                </div>
+                            </Tooltip>
 
-                        <Tooltip placement="left" title={"Edit Solution"}>
-                            <div className="action">
-                                <MdOutlineEdit color="#3A3E3E" />
-                            </div>
-                        </Tooltip>
+                            <Tooltip placement="left" title={"Edit Solution"}>
+                                <div className="action">
+                                    <MdOutlineEdit color="#3A3E3E" />
+                                </div>
+                            </Tooltip>
+                        </div>
+                        
                     </Col>
                 </Row>
 
